5f299c022fed142257ba4ab6d67f2142593c3413
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2024-05-14 21:16:13 -0300

    fix

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "eks_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
-  enable_dns_hostnames = tru
+  enable_dns_hostnames = true
   tags = {
     Name = "Curso Terraform - VPC"
   }
