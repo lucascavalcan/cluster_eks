b0b00d19eab7be137fdfc29651c1ec80e26c12de
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2024-05-14 21:01:34 -0300

    remote backend

--- provider.tf
+++ provider.tf
@@ -1,12 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.49.0"
     }
   }
+
+  backend "s3" {
+    bucket = "curso-terraform-lucas"
+    key    = "dev/terraform.state"
+    region = "us-east-1"
+  }
 }
 
-provider "aws" { # configurações específicas
-  region = "us_east_1"
+provider "aws" {
+  region = "us-east-1"
 }
