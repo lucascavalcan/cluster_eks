38c6ed41173fd38d6d724c8daba1d66deadbd9d4
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2024-05-16 22:07:23 -0300

    public route table

--- public.tf
+++ public.tf
@@ -25,3 +25,13 @@ resource "aws_subnet" "eks_subnet_public_1b" {
     }
   )
 }
+
+resource "aws_route_table_association" "eks_rtb_assoc_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
