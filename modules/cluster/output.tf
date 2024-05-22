19a56d1c783b902bb2f41a4568e5ffdd0bf0b26a
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2024-05-21 22:58:19 -0300

    mnd module

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -5,3 +5,7 @@ output "eks_vpc_config" {
 output "oidc" {
   value = flatten([for cert in data.tls_certificate.eks_oidc_tls_certificate.certificates : cert.sha1_fingerprint])
 }
+
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.id
+}
---
+++ modules/managed-node-group/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to name the resources"
+}
+
+variable "tags" {
+  type = map
+  description = "tags to be add to AWS resources"
+}
+
+variable "cluster_name" {
+    type = string
+    description = "EKS cluster name to create MNG"
+}
+
+variable "subnet_private_1a" {
+    type = string
+    description = "subnet id from AZ 1a"
+}
+
+variable "subnet_private_1b" {
+    type = string
+    description = "subnet id from AZ 1b"
+}
